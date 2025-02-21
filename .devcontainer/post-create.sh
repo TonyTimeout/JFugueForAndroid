979390099762a1d0ce2222588dfbe71f18d88272
TonyTimeout <tonytimetown@gmail.com>
2025-02-21 09:04:14 +0000

    started updating and get working in Codespaces

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 # Install essential packages
-apt update && apt install -y \
+sudo apt update && sudo apt install -y \
     curl \
     unzip \
     wget \
-    && apt clean
+    locate \
+    && sudo apt clean
 
 # Set environment variables for Android SDK
 export ANDROID_SDK_ROOT=/usr/local/android-sdk-linux
 export PATH=${PATH}:${ANDROID_SDK_ROOT}/cmdline-tools/tools/bin:${ANDROID_SDK_ROOT}/platform-tools
 
+# Create directory for Android SDK
+sudo mkdir -p ${ANDROID_SDK_ROOT}/cmdline-tools
+
 # Download and install Android SDK command line tools
-mkdir -p ${ANDROID_SDK_ROOT}/cmdline-tools \
-    && cd ${ANDROID_SDK_ROOT}/cmdline-tools \
-    && wget https://dl.google.com/android/repository/commandlinetools-linux-7583922_latest.zip -O commandlinetools.zip \
-    && unzip commandlinetools.zip \
-    && rm commandlinetools.zip
+cd ${ANDROID_SDK_ROOT}/cmdline-tools \
+    && sudo wget https://dl.google.com/android/repository/commandlinetools-linux-7583922_latest.zip -O commandlinetools.zip \
+    && sudo unzip commandlinetools.zip \
+    && sudo rm commandlinetools.zip
 
 # Install Android SDK packages
-yes | sdkmanager --licenses \
-    && sdkmanager --update \
-    && sdkmanager "platform-tools" \
+yes | sudo ${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager --licenses \
+    && sudo ${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager --update \
+    && sudo ${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager "platform-tools" \
     "platforms;android-30" \
     "build-tools;30.0.3"
